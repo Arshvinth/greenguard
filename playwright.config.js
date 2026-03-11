bcf36902655e47cef12705beded14e0884163a9b
Arshvinth <IT23373402@my.sliit.lk>
2026-03-11 15:57:40 +0530

    Removed CI/CD tests

--- playwright.config.js
+++ playwright.config.js
@@ -1,14 +1,10 @@
 const { defineConfig } = require('@playwright/test');
 
-// Detect if running in GitHub Actions
-const isCI = !!process.env.GITHUB_ACTIONS;
-
 module.exports = defineConfig({
   testDir: './tests',   // 👈 VERY IMPORTANT
 
   use: {
     baseURL: 'http://localhost:3000',
-     // If running in GitHub Actions, run headless; otherwise show browser
-    headless: isCI ? true : false,
+    headless: false,
   },
 });
